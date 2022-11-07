ce4f9ec1c4667bf5c19954abcb7787e57ded2d39
Kariba Yasmin <kariba.singularity@gmail.com>
2022-11-07 09:48:19 +0600

    [changed] CarouselList type and switch activity in MainActivity

--- app/src/main/java/com/kariba/prayheal/activity/MainActivity.kt
+++ app/src/main/java/com/kariba/prayheal/activity/MainActivity.kt
@@ -1,5 +1,6 @@
-package com.kariba.prayheal
+package com.kariba.prayheal.activity
 
+import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.TypedValue
@@ -8,18 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearSnapHelper
 import androidx.recyclerview.widget.SnapHelper
+import com.google.gson.Gson
+import com.kariba.prayheal.R
 import com.kariba.prayheal.adapter.AdapterCarouselView
 import com.kariba.prayheal.databinding.ActivityMainBinding
-import com.kariba.prayheal.interfaces.OnClick
+import com.kariba.prayheal.interfaces.OnClickListener
+import com.kariba.prayheal.models.CarouselResponse
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var carouselAdapter : AdapterCarouselView
+    private lateinit var carouselAdapter: AdapterCarouselView
 
-    var carouselList : ArrayList<String> = ArrayList()
+    var carouselList: ArrayList<CarouselResponse> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,14 +34,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        carouselList.add("Demo 1st Name")
-        carouselList.add("Demo 2nd Name")
-        carouselList.add("Demo 3rd Name")
-        carouselList.add("Demo 4th Name")
-        carouselList.add("Demo 5th Name")
-        carouselList.add("Demo 6th Name")
+        carouselList.add(CarouselResponse(1, "Demo 1st Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
+        carouselList.add(CarouselResponse(2, "Demo 2nd Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
+        carouselList.add(CarouselResponse(3, "Demo 3rd Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
+        carouselList.add(CarouselResponse(4, "Demo 4th Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
+        carouselList.add(CarouselResponse(5, "Demo 5th Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
+        carouselList.add(CarouselResponse(6, "Demo 6th Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
 
-        var snapHelper : SnapHelper = LinearSnapHelper()
+        var snapHelper: SnapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(recyclerViewCarousel)
 
         val r: Resources = resources
@@ -58,9 +62,14 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    var itemClick = object : OnClick {
+    private var itemClick = object : OnClickListener {
         override fun onClick(view: View, position: Int) {
-
+            var intent = Intent(this@MainActivity, FragmentActivity::class.java)
+            var bundle = Bundle()
+            bundle.putString("fragment", "carouselFragment")
+            bundle.putString("carouselItem", Gson().toJson(carouselList[position]))
+            intent.putExtras(bundle)
+            startActivity(intent)
         }
 
     }
