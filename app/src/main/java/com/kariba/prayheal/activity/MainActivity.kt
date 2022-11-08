bb548635d6d021a9311aea453a2e5b1c289d32da
Kariba Yasmin <kariba.singularity@gmail.com>
2022-11-08 10:10:54 +0600

    [Added] api call implementation in Main Activity

--- app/src/main/java/com/kariba/prayheal/activity/MainActivity.kt
+++ app/src/main/java/com/kariba/prayheal/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package com.kariba.prayheal.activity
 import android.content.Intent
 import android.content.res.Resources
 import android.os.Bundle
+import android.util.Log
 import android.util.TypedValue
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -15,7 +16,14 @@ import com.kariba.prayheal.adapter.AdapterCarouselView
 import com.kariba.prayheal.databinding.ActivityMainBinding
 import com.kariba.prayheal.interfaces.OnClickListener
 import com.kariba.prayheal.models.CarouselResponse
+import com.kariba.prayheal.network.ApiClient
+import com.kariba.prayheal.network.ApiHandler
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,7 +31,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var carouselAdapter: AdapterCarouselView
 
-    var carouselList: ArrayList<CarouselResponse> = ArrayList()
+    var carouselList: ArrayList<CarouselResponse.CarouselData.SurahData> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,12 +42,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initView() {
-        carouselList.add(CarouselResponse(1, "Demo 1st Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
-        carouselList.add(CarouselResponse(2, "Demo 2nd Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
-        carouselList.add(CarouselResponse(3, "Demo 3rd Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
-        carouselList.add(CarouselResponse(4, "Demo 4th Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
-        carouselList.add(CarouselResponse(5, "Demo 5th Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
-        carouselList.add(CarouselResponse(6, "Demo 6th Name", "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"))
+
+        loadCarouselData()
 
         var snapHelper: SnapHelper = LinearSnapHelper()
         snapHelper.attachToRecyclerView(recyclerViewCarousel)
@@ -57,9 +61,45 @@ class MainActivity : AppCompatActivity() {
         recyclerViewCarousel.adapter = carouselAdapter
         recyclerViewCarousel.setHasFixedSize(true)
 
+    }
+
+    private fun loadCarouselData() {
+        var call = ApiClient.getInstance("")?.create(ApiHandler::class.java)?.getQuran()
+
+        call?.enqueue(object : Callback<CarouselResponse>{
+            override fun onResponse(
+                call: Call<CarouselResponse>,
+                response: Response<CarouselResponse>
+            ) {
+                if(response.isSuccessful && response.body() != null){
+                    response.body()?.let { setCarouselResponseResponse(it) }
+                }else{
+                    if(response.errorBody() == null){
+                        setCarouselResponseResponse(CarouselResponse(response.code(), response.message()))
+                    }else{
+                        try {
+                            val jObjError = JSONObject(response.errorBody()!!.string())
+                            setCarouselResponseResponse(CarouselResponse(response.code(), jObjError.getString("message") ?: response.message()))
+                        }catch (e: Exception){
+                            setCarouselResponseResponse(CarouselResponse(response.code(), e.message.toString()))
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<CarouselResponse>, t: Throwable) {
+                setCarouselResponseResponse(CarouselResponse(100, t.message))
+            }
+
+        })
+    }
+
+    fun setCarouselResponseResponse(data : CarouselResponse){
+        carouselList.clear()
+        carouselList = data.carouselData?.surahList ?: ArrayList()
+
         carouselAdapter.setCarouselList(carouselList)
-
-
+        carouselAdapter.notifyDataSetChanged()
     }
 
     private var itemClick = object : OnClickListener {
