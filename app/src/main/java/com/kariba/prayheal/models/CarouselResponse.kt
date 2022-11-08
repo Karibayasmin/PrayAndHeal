dfa4b2c024b570aab174a4dbe321472bbd55b1cb
Kariba Yasmin <kariba.singularity@gmail.com>
2022-11-08 10:15:48 +0600

    [Update] CarouselResponse modified

--- app/src/main/java/com/kariba/prayheal/models/CarouselResponse.kt
+++ app/src/main/java/com/kariba/prayheal/models/CarouselResponse.kt
@@ -3,12 +3,97 @@ package com.kariba.prayheal.models
 import android.content.Context
 import android.graphics.drawable.Drawable
 import androidx.core.content.ContextCompat
+import com.google.gson.annotations.SerializedName
 import com.kariba.prayheal.R
 import java.io.Serializable
 
-class CarouselResponse (
-    var id: Int? = 0,
-    var name: String? = "",
-    var image: String? = "https://images.unsplash.com/photo-1478760329108-5c3ed9d495a0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
 
-    ):Serializable
+class CarouselResponse (
+    @SerializedName("code")
+    var responseCode : Int? = 0,
+
+    @SerializedName("status")
+    var userMessage : String? = "",
+
+    @SerializedName("data")
+    var carouselData : CarouselData? = CarouselData(),
+
+    ):Serializable{
+        class CarouselData(
+            @SerializedName("surahs")
+            var surahList : ArrayList<SurahData>? = ArrayList(),
+
+            @SerializedName("edition")
+            var edition : Edition? = null
+
+        ): Serializable{
+            class SurahData(
+                @SerializedName("number")
+                var number : Int? = 0,
+
+                @SerializedName("name")
+                var name : String? = "",
+
+                @SerializedName("englishName")
+                var englishName : String? = "",
+
+                @SerializedName("englishNameTranslation")
+                var englishNameTranslation : String? = "",
+
+                @SerializedName("revelationType")
+                var revelationType : String? = "",
+
+                @SerializedName("ayahs")
+                var ayahsList : ArrayList<AyahsData> = ArrayList()
+
+            ):Serializable{
+                class AyahsData(
+                    @SerializedName("number")
+                    var number : Int? = 0,
+
+                    @SerializedName("text")
+                    var text : String? = "",
+
+                    @SerializedName("numberInSurah")
+                    var numberInSurah : Int? = 0,
+
+                    @SerializedName("juz")
+                    var juz : Int? = 0,
+
+                    @SerializedName("manzil")
+                    var manzil : Int? = 0,
+
+                    @SerializedName("page")
+                    var page : Int? = 0,
+
+                    @SerializedName("ruku")
+                    var ruku : Int? = 0,
+
+                    @SerializedName("hizbQuarter")
+                    var hizbQuarter : Int? = 0,
+                ):Serializable
+            }
+
+            class Edition(
+                @SerializedName("identifier")
+                var identifier : String? = "",
+
+                @SerializedName("language")
+                var language : String? = "",
+
+                @SerializedName("name")
+                var name : String? = "",
+
+                @SerializedName("englishName")
+                var englishName : String? = "",
+
+                @SerializedName("format")
+                var format : String? = "",
+
+                @SerializedName("type")
+                var type : String? = "",
+
+                ):Serializable
+        }
+
+    }
