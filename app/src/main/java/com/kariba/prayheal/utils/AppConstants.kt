0915a3b4afcc7d05e719fe7a25b446beecf671fd
Kariba Yasmin <kariba.singularity@gmail.com>
2022-11-08 10:35:10 +0600

    [Added] utils package

---
+++ app/src/main/java/com/kariba/prayheal/utils/AppConstants.kt
@@ -0,0 +1,5 @@
+package com.kariba.prayheal.utils
+
+object AppConstants {
+    const val BASE_URL = "http://api.alquran.cloud/v1/quran/"
+}
---
+++ app/src/main/java/com/kariba/prayheal/utils/EndPoints.kt
@@ -0,0 +1,6 @@
+package com.kariba.prayheal.utils
+
+object EndPoints {
+
+    const val API_GET_QURAN = "quran-uthmani"
+}
