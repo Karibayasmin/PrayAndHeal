573e609a2ce2c9f7a6fa50b8da51877b65842418
Kariba Yasmin <kariba.singularity@gmail.com>
2022-11-17 17:14:17 +0600

    [Added] Shared preference table name in AppConstants

--- app/src/main/java/com/kariba/prayheal/utils/AppConstants.kt
+++ app/src/main/java/com/kariba/prayheal/utils/AppConstants.kt
@@ -2,4 +2,6 @@ package com.kariba.prayheal.utils
 
 object AppConstants {
     const val BASE_URL = "http://api.alquran.cloud/v1/quran/"
+
+    const val SHARED_PREF_TABLE_NAME = "shared_pref_table_name"
 }
